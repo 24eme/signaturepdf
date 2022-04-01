3e4aebac44fc5ff22edc261c7e8189f27c60b77d
Vincent LAURENT <vince.laurent@gmail.com>
2022-04-01 17:39:14 +0200

    Updating bootstrap

--- templates/signature.html.php
+++ templates/signature.html.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link href="/vendor/bootstrap.min.css?5.1.1" rel="stylesheet">
-    <link href="/vendor/bootstrap-icons.css?1.5.0" rel="stylesheet">
+    <link href="/vendor/bootstrap-icons.css?1.8.1" rel="stylesheet">
     <link href="/css/app.css" rel="stylesheet">
     <title>Signature PDF</title>
   </head>

801d5b48ddd38ff6354f216e873d6e858175b57f
Vincent LAURENT <vince.laurent@gmail.com>
2024-10-07 20:12:05 +0200

    Ajout de libre dans le nom de la pae pour le retrouvez plus facilement

--- templates/signature.html.php
+++ templates/signature.html.php
@@ -8,7 +8,7 @@
     <link href="<?php echo $REVERSE_PROXY_URL; ?>/vendor/bootstrap.<?php echo $DIRECTION_LANGUAGE ?>.min.css?5.3.3" rel="stylesheet">
     <link href="<?php echo $REVERSE_PROXY_URL; ?>/vendor/bootstrap-icons.min.css?1.11.3" rel="stylesheet">
     <link href="<?php echo $REVERSE_PROXY_URL; ?>/css/app.css?<?php echo ($COMMIT) ? $COMMIT : filemtime($ROOT."/public/css/app.css") ?>" rel="stylesheet">
-    <title>Signature PDF - Signer un PDF en ligne</title>
+    <title>Signature PDF Libre - Signer un PDF en ligne</title>
 </head>
 <body>
 <noscript>

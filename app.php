1dd10cb8f3989fa1be71a30960a5f037314577a0
Vincent LAURENT <vince.laurent@gmail.com>
2024-10-13 11:18:07 +0200

    Delete only if exist

--- app.php
+++ app.php
@@ -361,7 +361,9 @@ $f3->route('GET /cron', function($f3) {
         }
         $expiredFolder = str_replace('.expire', '', $expireFile);
         array_map('unlink', glob($expiredFolder."/*"));
-        unlink($expiredFolder.'/.lock');
+        if(file_exists($expiredFolder.'/.lock')) {
+            unlink($expiredFolder.'/.lock');
+        }
         rmdir($expiredFolder);
         unlink($expireFile);
     }

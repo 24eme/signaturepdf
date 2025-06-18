135d094a6a21c122d4006880f536265c548af6cd
Gabriel Poma <gpoma@24eme.fr>
2025-06-18 15:00:20 +0200

    sign color: "toolbox" with colorpicker

--- public/js/signature.js
+++ public/js/signature.js
@@ -157,6 +157,13 @@ async function loadPDF(pdfBlob) {
                   const textLinesMaxWidth = event.target.textLines.reduce((max, _, i) => Math.max(max, event.target.getLineWidth(i)), 0);
                   event.target.set({width: textLinesMaxWidth});
               });
+              canvasEdition.on("selection:created", function(event) {
+                  if (event.selected.length > 1 || event.selected.length === 0) {
+                      return;
+                  }
+
+                  toolBox.init(event.selected[0])
+              });
               canvasEditions.push(canvasEdition);
             });
         }
@@ -1118,3 +1125,52 @@ document.addEventListener('DOMContentLoaded', function () {
         window.location.reload();
     })
 });
+
+const toolBox = (function () {
+    const _coloricon = document.createElement('img')
+          _coloricon.src = 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-palette" viewBox="0 0 16 16"><path d="M8 5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3m4 3a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3M5.5 7a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m.5 6a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3"/><path d="M16 8c0 3.15-1.866 2.585-3.567 2.07C11.42 9.763 10.465 9.473 10 10c-.603.683-.475 1.819-.351 2.92C9.826 14.495 9.996 16 8 16a8 8 0 1 1 8-8m-8 7c.611 0 .654-.171.655-.176.078-.146.124-.464.07-1.119-.014-.168-.037-.37-.061-.591-.052-.464-.112-1.005-.118-1.462-.01-.707.083-1.61.704-2.314.369-.417.845-.578 1.272-.618.404-.038.812.026 1.16.104.343.077.702.186 1.025.284l.028.008c.346.105.658.199.953.266.653.148.904.083.991.024C14.717 9.38 15 9.161 15 8a7 7 0 1 0-7 7"/></svg>'
+
+    function _renderIcon(icon) {
+        return function renderIcon(ctx, left, top, styleOverride, fabricObject) {
+            const size = this.cornerSize;
+            ctx.save();
+            ctx.translate(left, top);
+            ctx.rotate(fabric.util.degreesToRadians(fabricObject.angle));
+            ctx.drawImage(icon, -size/2, -size/2, size, size);
+            ctx.restore();
+        }
+    }
+
+    function _changeColor(eventData, transform) {
+        const target = transform.target;
+        const _colorpicker = document.createElement('input')
+              _colorpicker.setAttribute('type', 'color')
+
+        _colorpicker.addEventListener('input', function (e) {
+            target.set({ fill: e.target.value })
+            target.canvas.requestRenderAll()
+        })
+
+        _colorpicker.click()
+        _colorpicker.remove()
+    }
+
+    function init(el) {
+        colorControl = new fabric.Control({
+            x: 0.5,
+            y: -0.5,
+            offsetY: -16,
+            offsetX: 16,
+            cursorStyle: 'pointer',
+            mouseUpHandler: _changeColor,
+            render: _renderIcon(_coloricon),
+            cornerSize: 24
+        })
+
+        el.controls.color = colorControl
+    }
+
+    return {
+        init: init
+    }
+})()
